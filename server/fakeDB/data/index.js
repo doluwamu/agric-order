b4f85ef875f11c4c5d773f757d2e4af83ea80cd4
Doluwamu <dboyadeitan@gmail.com>
2020-12-17 18:30:33 +0100

    made improvements in server

--- server/fakeDB/data/index.js
+++ server/fakeDB/data/index.js
@@ -27,8 +27,9 @@ exports.users = [
 
 exports.products = [
   {
-    name: "Cow",
-    image: "/images/cat",
+    name: "cow",
+    image:
+      "https://www.google.com/imgres?imgurl=http%3A%2F%2Fs3-wp-lyleprintingandp.netdna-ssl.com%2Fwp-content%2Fuploads%2F2018%2F01%2F09060054%2Fcow-354428_1280.jpg&imgrefurl=https%3A%2F%2Fwww.farmanddairy.com%2Ftop-stories%2Fhow-to-determine-when-a-cow-is-in-heat%2F464746.html&tbnid=PyIx9TqmM3IGAM&vet=12ahUKEwipzIzi6dXtAhXT_4UKHWezAQ0QMygCegUIARDRAQ..i&docid=Fdo_BS4oFDoRzM&w=1280&h=982&q=cow&ved=2ahUKEwipzIzi6dXtAhXT_4UKHWezAQ0QMygCegUIARDRAQ",
     category: "cow",
     price: 150000,
     ratings: 4.5,
@@ -39,8 +40,9 @@ exports.products = [
     available: true,
   },
   {
-    name: "Goat",
-    image: "/images/cat",
+    name: "goat",
+    image:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn-a.william-reed.com%2Fvar%2Fwrbm_gb_food_pharma%2Fstorage%2Fimages%2Fpublications%2Ffood-beverage-nutrition%2Ffoodnavigator-asia.com%2Fheadlines%2Fmarkets%2Fgetting-the-goat-australia-struggling-to-meet-soaring-demand-for-goat-meat%2F9022285-1-eng-GB%2FGetting-the-goat-Australia-struggling-to-meet-soaring-demand-for-goat-meat_wrbm_large.jpg&imgrefurl=https%3A%2F%2Fwww.foodnavigator-asia.com%2FArticle%2F2019%2F01%2F21%2FGetting-the-goat-Australia-struggling-to-meet-soaring-demand-for-goat-meat&tbnid=9a5W-c0dKLJH-M&vet=12ahUKEwjS88316dXtAhULHBoKHTHaDhcQMygNegUIARDrAQ..i&docid=KDlbUJKGR3qnlM&w=749&h=530&q=goat&ved=2ahUKEwjS88316dXtAhULHBoKHTHaDhcQMygNegUIARDrAQ",
     category: "goat",
     price: 30000,
     ratings: 4.0,
@@ -51,8 +53,8 @@ exports.products = [
     available: true,
   },
   {
-    name: "Ram",
-    image: "/images/cat",
+    name: "ram",
+    image: "https://i.ytimg.com/vi/BFMpZB88hho/maxresdefault.jpg",
     category: "sheep",
     price: 45000,
     ratings: 4.2,
