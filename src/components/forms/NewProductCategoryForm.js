c2e139875208b22aa6977757062e75d55a83727a
Doluwamu <dboyadeitan@gmail.com>
2021-01-06 00:28:35 +0100

    added new feature

---
+++ src/components/forms/NewProductCategoryForm.js
@@ -0,0 +1,81 @@
+// import { ServerError } from "errors/Server";
+import { RequiredField } from "helpers/FormMessage";
+import React from "react";
+import { useForm } from "react-hook-form";
+// import { Link } from "react-router-dom";
+
+// eslint-disable-next-line
+
+const NewProductCategoryForm = ({ onSubmit }) => {
+  const { register, errors, handleSubmit } = useForm();
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="login-form forms">
+      <div className="form_header">
+        <h2>Create a new product category</h2>
+      </div>
+
+      <div className="category_name form_part">
+        <div className="form_label">
+          <label>Category Name:</label>
+        </div>
+        <input
+          ref={register({ required: true })}
+          type="text"
+          name="categoryName"
+          className="category_name_input"
+          id="category_name"
+          placeholder="Enter a name for the category..."
+        />
+        {errors.categoryName && (
+          <div className="alert alert-danger">
+            {errors.categoryName.type === "required" && <RequiredField />}
+          </div>
+        )}
+      </div>
+
+      <div className="category_type form_part">
+        <div className="form_label">
+          <label>Select category type:</label>
+        </div>
+        <select
+          ref={register({ required: true })}
+          name="categoryType"
+          className="category_type_input"
+          id="category_type"
+        >
+          <option>Animal</option>
+          <option>Plant</option>
+          <option>Machinery</option>
+          <option>Other</option>
+        </select>
+
+        {errors.categoryType && (
+          <div className="alert alert-danger">
+            {errors.categoryType.type === "required" && <RequiredField />}
+          </div>
+        )}
+      </div>
+
+      <div className="form_button">
+        <button type="submit" className="btn btn-secondary">
+          Create
+        </button>
+      </div>
+
+      <div style={{ marginTop: "20px", lineHeight: "23px", fontSize: "17px" }}>
+        <p>
+          Please do not create a category with a similar category already
+          existing.
+        </p>
+        <p>For example, creating cow category when cattle already exists.</p>
+        <p>Thank you.</p>
+      </div>
+
+      {/* <div className="form_part">
+        <ServerError error={error} />
+      </div> */}
+    </form>
+  );
+};
+
+export default NewProductCategoryForm;
--- src/pages/NewProductCategoryPage.js
+++ src/pages/NewProductCategoryPage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+// import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import NewProductCategoryForm from "components/forms/NewProductCategoryForm";
 
-const NewProductCategoryPage = () => {
-  return <div>hello world</div>;
-};
+class NewProductCategoryPage extends Component {
+  // state = {
+  //   shouldRedirect: false,
+  //   error: [],
+  // };
+  createProductCategory = (categoryData) => {
+    alert(JSON.stringify(categoryData));
+    // this.props.auth
+    //   .logIn(loginData)
+    //   .then((_) => {
+    //     this.setState({ shouldRedirect: true });
+    //   })
+    //   .catch((error) => {
+    //     return this.setState({ error });
+    //   });
+  };
+
+  render() {
+    // const { error, shouldRedirect } = this.state;
+
+    // if (shouldRedirect) {
+    //   return <Redirect to={{ pathname: "/" }} />;
+    // }
+
+    return (
+      <div className="form-section">
+        <NewProductCategoryForm onSubmit={this.createProductCategory} />
+      </div>
+    );
+  }
+}
 
 export default NewProductCategoryPage;
