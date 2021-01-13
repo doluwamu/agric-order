624d384d1f1ea86902ba2ceec24c36cd4941b1d5
Doluwamu <dboyadeitan@gmail.com>
2021-01-14 00:11:35 +0100

    made ui improvements

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -27,6 +27,12 @@ class ProductPage extends Component {
       <div className="all_products">
         <div className="all_products_body">
           <header className="products_header">Check out our animals</header>
+          <header
+            className="products_header"
+            style={{ fontSize: "20px", fontWeight: "bold" }}
+          >
+            Click on desired product to continue shopping
+          </header>
           <div className="products">
             <ProductCard products={products} />
           </div>
