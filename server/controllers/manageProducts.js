6c444eb18ca4532c03c24b77fa62bfeff8d939f6
Doluwamu <dboyadeitan@gmail.com>
2021-04-11 15:51:59 +0100

    made additions to manage feature

--- server/controllers/manageProducts.js
+++ server/controllers/manageProducts.js
@@ -1,10 +1,32 @@
-const Products = require("../models/product");
+const Product = require("../models/product");
 
 exports.getUserProducts = async (req, res) => {
   const { user } = res.locals;
   try {
-    const foundProducts = await Products.find({ owner: user });
+    const foundProducts = await Product.find({ owner: user });
     return res.json(foundProducts);
+  } catch (error) {
+    return res.mongoError(error);
+  }
+};
+
+exports.deleteUserProduct = async (req, res) => {
+  const { productId } = req.params;
+  const { user } = res.locals;
+
+  try {
+    const foundProduct = await Product.findById(productId).populate(
+      "owner",
+      "-password"
+    );
+    if (user.id !== foundProduct.owner.id) {
+      return res.sendApiError({
+        title: "Invalid user!",
+        detail: "You are not owner of this product!",
+      });
+    }
+    await foundProduct.remove();
+    return res.json({ id: productId });
   } catch (error) {
     return res.mongoError(error);
   }
--- server/routes/manageProducts.js
+++ server/routes/manageProducts.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 
 const { onlyAuthenticatedUser } = require("../controllers/users");
-const { getUserProducts } = require("../controllers/manageProducts");
+const {
+  getUserProducts,
+  deleteUserProduct,
+} = require("../controllers/manageProducts");
 
+router.delete(
+  "/delete-product/:productId",
+  onlyAuthenticatedUser,
+  deleteUserProduct
+);
 router.get("/my-products/", onlyAuthenticatedUser, getUserProducts);
 
 module.exports = router;
